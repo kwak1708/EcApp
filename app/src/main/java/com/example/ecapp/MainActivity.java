44147117daebc8e06d5aeb11e40bc57549f87822
mhkwak <hyouk1708@gmail.com>
2021-07-27 22:14:00 +0900

    commit

--- app/src/main/java/com/example/ecapp/MainActivity.java
+++ app/src/main/java/com/example/ecapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ecapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,4 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();//logout
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+
 }
